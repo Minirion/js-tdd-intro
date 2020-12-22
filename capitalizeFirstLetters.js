c016a919e8521e2af5cb6a1b27a5fab195019e00
Minirion <deschampsm.pro@gmail.com>
2020-12-22 18:39:10 +0100

    script function

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,11 +1,19 @@
 const assert = require('assert');
 
+function capitalizeFirstLetters (input) {
+  let step1 = input.split(" ");
+  let step2 = step1.map((one) => one.charAt(0).toUpperCase() + one.slice(1));
+  let step3 = step2.join(" ");
+  return step3;
+};
+
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 
 assert.strictEqual(capitalizeFirstLetters('i love tdd'), 'I Love Tdd');
 
-assert.strictEqual(capitalizeFirst('coucou'), 'Coucou');
+assert.strictEqual(capitalizeFirstLetters('coucou'), 'Coucou');
 
-assert.strictEqual(capitalizeFirst(''), '');
+assert.strictEqual(capitalizeFirstLetters(''), '');
+
